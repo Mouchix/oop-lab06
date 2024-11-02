96d82e5f9e72482adbe03adb1aa9ac721902d9e5
Mondardini Marco <marco.mondardini2@studio.unibo.it>
2024-11-02 14:40:09 +0100

    final commit ex63 + update .gitignore file

---
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/NetworkException.java
@@ -0,0 +1,13 @@
+package it.unibo.exceptions.fakenetwork.impl;
+
+import java.io.IOException;
+
+public class NetworkException extends IOException{
+    NetworkException(){
+        super("Network error: no response");
+    }
+
+    NetworkException(String message){
+        super("Network error while sending message: <"+ message + ">");
+    }
+}
